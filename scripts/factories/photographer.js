463ad3186338d4ffc8553445bbf98e5016032c94
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2022-12-20 13:54:49 +0100

    Added alt txt generation to photographerFactory

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -2,15 +2,21 @@ export function photographerFactory(data) {
   const { name, portrait } = data;
 
   const picture = `assets/photographers/${portrait}`;
+  const imgAltText = `Portrait of ${name}`;
 
   function getUserCardDOM() {
     const article = document.createElement("article");
+
     const img = document.createElement("img");
     img.setAttribute("src", picture);
+    img.setAttribute("alt", imgAltText);
+
     const h2 = document.createElement("h2");
     h2.textContent = name;
+
     article.appendChild(img);
     article.appendChild(h2);
+
     return article;
   }
   return { name, picture, getUserCardDOM };
