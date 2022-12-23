14180439962cb1c8864388110cb6d506cfac8f25
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2022-12-23 16:42:35 +0100

    Added functions to retrieve Media & Info from Id

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -3,6 +3,21 @@ import { fetchJsonData } from "../utils/fetchJsonData.js";
 
 const params = new URL(document.location).searchParams;
 const photographerId = parseInt(params.get("id"));
-const { photographers, media } = await fetchJsonData();
 
-console.log(photographerId);
+async function getPhotographerInfo() {
+  const { photographers } = await fetchJsonData();
+  return photographers.find(
+    (photographer) => photographer.id === photographerId
+  );
+}
+
+async function getPhotographerMedia() {
+  const { media } = await fetchJsonData();
+  return media.filter(
+    (mediaItem) => mediaItem.photographerId === photographerId
+  );
+}
+
+console.log(getPhotographerInfo());
+
+console.log(getPhotographerMedia());
