2896320ed5a4516fb04c2c43748f3ca290dfb821
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2022-12-26 11:11:40 +0100

    Added comments to index.js

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,19 +1,29 @@
 import { photographerFactory } from "../factories/photographerFactory.js";
 import { fetchJsonData } from "../utils/fetchJsonData.js";
 
+// Generates the HTML for each photographer
 async function generatePhotographerHtml(photographers) {
+  // Get the section element that will contain the photographer cards
   const photographersSection = document.querySelector(".photographer-section");
 
+  // Loop through the photographers array and create a card for each one
   photographers.forEach((photographer) => {
+    // Create a photographer model object from the data
     const photographerModel = photographerFactory(photographer);
+    // Get the DOM element for the photographer card
     const userCardDOM = photographerModel.getPhotographerCardDOM();
+    // Add the card to the photographers section
     photographersSection.appendChild(userCardDOM);
   });
 }
 
+// Fetches the photographer data and renders the cards
 async function renderPhotographerProfiles() {
+  // Get the data for the photographers
   const { photographers } = await fetchJsonData();
+  // Generate the HTML for the photographer cards
   generatePhotographerHtml(photographers);
 }
 
+// Render all photographer profiles
 renderPhotographerProfiles();
