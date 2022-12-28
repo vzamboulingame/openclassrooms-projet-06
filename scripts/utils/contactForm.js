e98d236484ae3725983c8edd0a93e700d15a3edd
Vinodh Zamboulingame <vzamboulingame@gmail.com>
2022-12-28 14:10:49 +0100

    Added aria-hidden to displayModal & closeModal

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,15 @@
 export function displayModal() {
-  const modal = document.querySelector(".contact-modal");
+  const modal = document.getElementById("contactModal");
+  const main = document.querySelector("main");
   modal.style.display = "block";
+  modal.setAttribute("aria-hidden", "false");
+  main.setAttribute("aria-hidden", "true");
 }
 
 export function closeModal() {
-  const modal = document.querySelector(".contact-modal");
+  const modal = document.getElementById("contactModal");
+  const main = document.querySelector("main");
   modal.style.display = "none";
+  main.setAttribute("aria-hidden", "false");
+  modal.setAttribute("aria-hidden", "true");
 }
